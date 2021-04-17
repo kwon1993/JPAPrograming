899a65830553ebf6e5f3acc8910b610ca01c21bb
kwon oyeop <wdmono@naver.com>
2021-04-17 19:24:39 +0900

    객체지향 쿼리 언어 중급 문법

--- study/jpql/src/main/java/jpql/JpaMain.java
+++ study/jpql/src/main/java/jpql/JpaMain.java
@@ -14,26 +14,41 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
 
-            member.setTeam(team);
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
 
-            em.persist(member);
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select nullif(m.username, '관리자') from Member m";
-            List<String> resultList = em.createQuery(query, String.class).getResultList();
-            for (String s : resultList) {
-                System.out.println("s = " + s);
-            }
+            int resultCount1 = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+            System.out.println("resultCount = " + resultCount1);
+
+            String qlString = "update Product p set p.price = p.price * 1.1 where p.stockAmount < :stockAmount";
+            int resultCount = em.createQuery(qlString)
+                    .setParameter("stockAmount", 10)
+                    .executeUpdate();
+
+            System.out.println("resultCount = " + resultCount);
 
             tx.commit();
         } catch (Exception e){
